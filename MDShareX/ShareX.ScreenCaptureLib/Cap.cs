60b917e12f3e7624d438270966b415148d7111d7
Raven <ravenzyj@outlook.com>
2016-06-07 10:23:31 +0800

    init lib

---
+++ MDShareX/ShareX.ScreenCaptureLib/Cap.cs
@@ -0,0 +1,83 @@
+﻿using ShareX.HelpersLib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ShareX.ScreenCaptureLib
+{
+    class Cap
+    {
+
+        public delegate Image ScreenCaptureDelegate();
+
+        public void DoCapture(ScreenCaptureDelegate capture, CaptureType captureType, TaskSettings taskSettings = null, bool autoHideForm = true)
+        {
+            if (taskSettings == null) taskSettings = TaskSettings.GetDefaultTaskSettings();
+
+            if (taskSettings.CaptureSettings.IsDelayScreenshot && taskSettings.CaptureSettings.DelayScreenshot > 0)
+            {
+                TaskEx.Run(() =>
+                {
+                    int sleep = (int)(taskSettings.CaptureSettings.DelayScreenshot * 1000);
+                    Thread.Sleep(sleep);
+                },
+                () =>
+                {
+                    DoCaptureWork(capture, captureType, taskSettings, autoHideForm);
+                });
+            }
+            else
+            {
+                DoCaptureWork(capture, captureType, taskSettings, autoHideForm);
+            }
+        }
+
+
+        public void DoCaptureWork(ScreenCaptureDelegate capture, CaptureType captureType, TaskSettings taskSettings, bool autoHideForm = true)
+        {
+            //if (autoHideForm)
+            //{
+            //    Hide();
+            //    Thread.Sleep(250);
+            //}
+
+            Image img = null;
+
+            try
+            {
+                Screenshot.CaptureCursor = taskSettings.CaptureSettings.ShowCursor;
+                Screenshot.CaptureShadow = taskSettings.CaptureSettings.CaptureShadow;
+                Screenshot.ShadowOffset = taskSettings.CaptureSettings.CaptureShadowOffset;
+                Screenshot.CaptureClientArea = taskSettings.CaptureSettings.CaptureClientArea;
+                Screenshot.AutoHideTaskbar = taskSettings.CaptureSettings.CaptureAutoHideTaskbar;
+
+                img = capture();
+            }
+            catch (Exception ex)
+            {
+                DebugHelper.WriteException(ex);
+            }
+            finally
+            {
+                if (autoHideForm)
+                {
+                    //this.ForceActivate();
+                }
+
+                AfterCapture(img, captureType, taskSettings);
+            }
+        }
+
+        public void AfterCapture(Image img, CaptureType captureType, TaskSettings taskSettings)
+        {
+            if (img != null)
+            {
+            
+            }
+        }
+
+    }
+}
---
+++ MDShareX/ShareX.ScreenCaptureLib/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace ShareX.ScreenCaptureLib
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            BaseRegionForm form;
+            TaskSettings taskSettings = TaskSettings.GetDefaultTaskSettings();
+
+            RectangleRegionMode mode = RectangleRegionMode.Annotation;
+            form = new RectangleRegionForm(mode);
+
+            var cap = new Cap();
+            cap.DoCapture(() =>
+            {
+                Image img = null;
+
+                try
+                {
+                    form.Config = taskSettings.CaptureSettingsReference.SurfaceOptions;
+                    form.Prepare();
+                    form.ShowDialog();
+
+                    img = form.GetResultImage();
+
+                    if (img != null)
+                    {
+                        if (form.Result == RegionResult.Region && taskSettings.UploadSettings.RegionCaptureUseWindowPattern)
+                        {
+                            WindowInfo windowInfo = form.GetWindowInfo();
+
+                            if (windowInfo != null)
+                            {
+                                img.Tag = new ImageTag
+                                {
+                                    WindowTitle = windowInfo.Text,
+                                    ProcessName = windowInfo.ProcessName
+                                };
+                            }
+                        }
+
+                        lastRegionCaptureType = LastRegionCaptureType.Default;
+                    }
+                }
+                finally
+                {
+                    if (form != null)
+                    {
+                        form.Dispose();
+                    }
+                }
+
+                return img;
+            }, captureType,taskSettings,autoHideForm);
+
+
+            Application.Run(new ());
+        }
+
+     
+
+
+        /*
+        CaptureScreenshot(CaptureType.Rectangle); 
+        CaptureRegion(captureType, null, null);
+        RectangleRegionMode mode = taskSettings.CaptureSettings.SurfaceOptions.AnnotationEnabled ? RectangleRegionMode.Annotation : RectangleRegionMode.Default;
+        form = new RectangleRegionForm(mode);
+         
+         */
+
+
+
+
+
+
+
+
+
+    }
+}
